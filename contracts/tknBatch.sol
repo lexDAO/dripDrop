95f1701d59e6febaf1540039bb19105d12dddd61
Erich Dylus <51535743+sulyde@users.noreply.github.com>
2020-04-20 09:09:38 -0400

    Update tknBatch.sol
    Update latest version of IERC20

--- contracts/tknBatch.sol
+++ contracts/tknBatch.sol
@@ -156,8 +156,7 @@ library SafeMath {
 }
 
 /**
- * @dev Interface of the ERC20 standard as defined in the EIP. Does not include
- * the optional functions; to access them see {ERC20Detailed}.
+ * @dev Interface of the ERC20 standard as defined in the EIP. 
  */
 interface IERC20 {
     /**
