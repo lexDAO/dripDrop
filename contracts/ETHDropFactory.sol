c9b7957a558da918f6805dff22c6c755734fb7b5
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-04-21 21:49:46 -0400

    Update ETHDropFactory.sol
    add token overlay and syntax uberfication

--- contracts/ETHDropFactory.sol
+++ contracts/ETHDropFactory.sol
@@ -1,10 +1,18 @@
 pragma solidity 0.5.14;
 
-contract ETHDrop {
-    /*******
-    INTERNAL
-    *******/
-    uint256 public drip;
+interface IToken { // brief ERC-20 interface
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+}
+
+contract MemberDripDrop {
+    /***************
+    INTERNAL DETAILS
+    ***************/
+    uint256 public ethDrip;
+    uint256 public tokenDrip;
+    IToken public dripToken;
     address payable[] members;
     address payable public secretary;
     
@@ -20,25 +28,64 @@ contract ETHDrop {
         _;
     }
     
+    // ******
+    // EVENTS
+    // ******
+    event DripTokenUpdated(address indexed updatedDripToken);
+    event TokenDripUpdated(uint256 indexed updatedTokenDrip);
+    event ETHDripUpdated(uint256 indexed updatedETHDrip);
+    event MemberAdded(address indexed addedMember);
+    event MemberRemoved(address indexed removedMember);
+    event SecretaryUpdated(address indexed updatedSecretary);
+    
     function() external payable { } // contract receives ETH
 
-    constructor(uint256 _drip, address payable[] memory _members) payable public { // initializes contract
+    constructor(
+        uint256 _ethDrip, 
+        uint256 _tokenDrip,  
+        address dripTokenAddress, 
+        address payable[] memory _members) payable public { // initializes contract
         for (uint256 i = 0; i < _members.length; i++) {
             require(_members[i] != address(0), "member address cannot be 0");
             memberList[_members[i]].memberIndex = members.push(_members[i]) - 1;
             memberList[_members[i]].exists = true;
         }
         
-        drip = _drip;
+        ethDrip = _ethDrip;
+        tokenDrip = _tokenDrip;
+        dripToken = IToken(dripTokenAddress);
         secretary = members[0]; // first address in member array is secretary  
     }
     
-    /******************
-    DRIP/DROP FUNCTIONS
-    ******************/
+    /************************
+    DRIP/DROP TOKEN FUNCTIONS
+    ************************/
+    function dripTKN() public onlySecretary { // transfer token to members per stored drip amount
+        for (uint256 i = 0; i < members.length; i++) {
+            dripToken.transfer(members[i], tokenDrip);
+        }
+    }
+    
+    function dropTKN(uint256 drop, address dropTokenAddress) public onlySecretary { // transfer token to members per approved drop amount
+        for (uint256 i = 0; i < members.length; i++) {
+            IToken dropToken = IToken(dropTokenAddress);
+            dropToken.transferFrom(msg.sender, members[i], drop);
+        }
+    }
+    
+    function customDropTKN(uint256[] memory drop, address dropTokenAddress) public onlySecretary { // transfer token to members per approved index drop amounts
+        for (uint256 i = 0; i < members.length; i++) {
+            IToken dropToken = IToken(dropTokenAddress);
+            dropToken.transferFrom(msg.sender, members[i], drop[i]);
+        }
+    }
+    
+    /**********************
+    DRIP/DROP ETH FUNCTIONS
+    **********************/
     function dripETH() public onlySecretary { // transfer ETH to members per stored drip amount
         for (uint256 i = 0; i < members.length; i++) {
-            members[i].transfer(drip);
+            members[i].transfer(ethDrip);
         }
     }
 
@@ -48,36 +95,23 @@ contract ETHDrop {
         }
     }
     
-    function customDropETH(uint256[] memory drop) payable public onlySecretary { // transfer ETH to members per index amounts
+    function customDropETH(uint256[] memory drop) payable public onlySecretary { // transfer ETH to members per index drop amounts
         for (uint256 i = 0; i < members.length; i++) {
             members[i].transfer(drop[i]);
         }
     }
-
-    function getETHBalance() public view returns (uint256) { // get amount of ETH in contract
-        return address(this).balance;
-    }
     
-    /***************
-    MEMBER FUNCTIONS
-    ***************/
-    function addMember(address payable newMember) public onlySecretary { 
-        require(memberList[newMember].exists != true, "member already exists");
-        memberList[newMember].memberIndex = members.push(newMember) - 1;
-        memberList[newMember].exists = true;
-    }
-
-    function getMembership() public view returns (address payable[] memory) {
-        return members;
-    }
-
-    function getMemberCount() public view returns(uint256 memberCount) {
-        return members.length;
-    }
-
-    function isMember(address memberAddress) public view returns (bool memberExists) {
-        if(members.length == 0) return false;
-        return (members[memberList[memberAddress].memberIndex] == memberAddress);
+    /******************
+    SECRETARY FUNCTIONS
+    ******************/
+    // ******************
+    // DRIP/DROP REGISTRY
+    // ******************
+    function addMember(address payable addedMember) public onlySecretary { 
+        require(memberList[addedMember].exists != true, "member already exists");
+        memberList[addedMember].memberIndex = members.push(addedMember) - 1;
+        memberList[addedMember].exists = true;
+        emit MemberAdded(addedMember);
     }
 
     function removeMember(address removedMember) public onlySecretary {
@@ -88,26 +122,80 @@ contract ETHDrop {
         memberList[keyToMove].memberIndex = memberToDelete;
         memberList[removedMember].exists = false;
         members.length--;
+        emit MemberRemoved(removedMember);
     }
 
-    function transferSecretary(address payable newSecretary) public onlySecretary {
-        secretary = newSecretary;
+    function updateSecretary(address payable updatedSecretary) public onlySecretary {
+        secretary = updatedSecretary;
+        emit SecretaryUpdated(updatedSecretary);
+    }
+    
+    // ************
+    // DRIP DETAILS
+    // ************
+    function updateETHDrip(uint256 updatedETHDrip) public onlySecretary {
+        ethDrip = updatedETHDrip;
+        emit ETHDripUpdated(updatedETHDrip);
+    }
+    
+    function updateDripToken(address updatedDripToken) public onlySecretary {
+        dripToken = IToken(updatedDripToken);
+        emit DripTokenUpdated(updatedDripToken);
+    }
+    
+    function updateTokenDrip(uint256 updatedTokenDrip) public onlySecretary {
+        tokenDrip = updatedTokenDrip;
+        emit TokenDripUpdated(updatedTokenDrip);
+    }
+    
+    /***************
+    GETTER FUNCTIONS
+    ***************/
+    // ****
+    // DRIP
+    // ****
+    function ETHBalance() public view returns (uint256) { // get balance of ETH in contract
+        return address(this).balance;
+    }
+    
+    function TokenBalance() public view returns (uint256) { // get balance of drip token in contract
+        return dripToken.balanceOf(address(this));
     }
 
-    function updateDrip(uint256 newDrip) public onlySecretary {
-        drip = newDrip;
+    // ******
+    // MEMBER
+    // ******
+    function Membership() public view returns (address payable[] memory) {
+        return members;
+    }
+
+    function MemberCount() public view returns(uint256 memberCount) {
+        return members.length;
+    }
+
+    function isMember(address memberAddress) public view returns (bool memberExists) {
+        if(members.length == 0) return false;
+        return (members[memberList[memberAddress].memberIndex] == memberAddress);
     }
 }
 
-contract ETHDropFactory {
-    ETHDrop private Drop;
-    address[] public drops;
+contract MemberDripDropFactory {
+    MemberDripDrop private DripDrop;
+    address[] public dripdrops;
 
-    event newDrop(address indexed secretary, address indexed drop);
+    event newDripDrop(address indexed secretary, address indexed dripdrop);
 
-    function newETHDrop(uint256 _drip, address payable[] memory _members) payable public {
-        Drop = (new ETHDrop).value(msg.value)(_drip, _members);
-        drops.push(address(Drop));
-        emit newDrop(_members[0], address(Drop));
+    function newMemberDripDrop(
+        uint256 _ethDrip, 
+        uint256 _tokenDrip,  
+        address dripTokenAddress, 
+        address payable[] memory _members) payable public {
+        DripDrop = (new MemberDripDrop).value(msg.value)(
+            _ethDrip,
+            _tokenDrip,
+            dripTokenAddress,
+            _members);
+        dripdrops.push(address(DripDrop));
+        emit newDripDrop(_members[0], address(DripDrop));
     }
 }
