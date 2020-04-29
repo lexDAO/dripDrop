bcd3c3c67696dfdec4bd8049bda03634a4ac98ca
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-04-29 12:22:25 -0400

    Update dropETH.sol
    lil more space shifting

--- contracts/dropETH.sol
+++ contracts/dropETH.sol
@@ -5,7 +5,7 @@ contract DropETH {
     function dropETH(address payable[] memory recipients) public payable {
         uint256 amount = msg.value / recipients.length;
         for (uint256 i = 0; i < recipients.length; i++) {
-	     recipients[i].transfer(amount);
+	    recipients[i].transfer(amount);
         }
     }
 }
