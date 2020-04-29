71d5a0c50a163dc6bf1d226fbc28dcc9e2e1c105
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-04-29 16:45:09 -0400

    Update and rename dropToken.sol to customDropToken.sol
    adjust to better fit dripDrop naming convention

--- contracts/customDropToken.sol
+++ contracts/customDropToken.sol
@@ -4,9 +4,9 @@ interface IToken {
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 }
 
-contract dropToken {
-    // transfer msg.sender token to recipients per approved drop amount 
-    function dropTKN(uint256[] memory amounts, address tokenAddress, address[] memory recipients) public {
+contract customDropToken {
+    // transfer msg.sender token to recipients per approved index drop amounts
+    function customDropTKN(uint256[] memory amounts, address tokenAddress, address[] memory recipients) public {
         IToken token = IToken(tokenAddress);
         for (uint256 i = 0; i < recipients.length; i++) {
 	    token.transferFrom(msg.sender, recipients[i], amounts[i]);
