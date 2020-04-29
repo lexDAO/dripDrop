8aff2de8e9a81515b90dfdbe6ef5fef074cc93b3
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-04-29 12:19:06 -0400

    Update dropETH.sol
    space saving scrunch

--- contracts/dropETH.sol
+++ contracts/dropETH.sol
@@ -4,9 +4,8 @@ contract DropETH {
     // transfer msg.sender ETH to recipients per attached drop amount 
     function dropETH(address payable[] memory recipients) public payable {
         uint256 amount = msg.value / recipients.length;
-
         for (uint256 i = 0; i < recipients.length; i++) {
-	        recipients[i].transfer(amount);
+	     recipients[i].transfer(amount);
         }
     }
 }
